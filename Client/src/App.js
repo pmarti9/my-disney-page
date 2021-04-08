b7091a0a12f9f2715733c88cd039eb003540e1ce
pmarti99 <parkerjmartin883@gmail.com>
2021-04-08 09:26:18 -0600

    heroku push

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import DisneyArticles from "../src/components/DisneyArticles/DisneyArticles"
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
       <h1>Disney App!</h1>
+      <DisneyArticles />
+      <Navigation />
     </div>
   );
 }
---
+++ Client/src/components/DisneyArticles/DisneyArticles.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function disneyArticles() {
+
+    return (
+    <div>
+        <h1>This is the articles component!!</h1>
+    </div>
+    )
+}
+export default disneyArticles
